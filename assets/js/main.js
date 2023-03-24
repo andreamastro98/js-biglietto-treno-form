d23ace065b6d3117a9b94daa21654a781c2bbfef
andreamastro98 <andreamastrovito98@gmail.com>
2023-03-24 11:51:55 +0100

    correzioni di java

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,23 +14,31 @@ function CalcoloCostoBiglietto(){
     
     if (EtaPasseggero < 18){
         CostoBiglietto = ( (0.21 * Chilometri) * 0.8).toFixed(2)
-        console.log(`Il costo del biglietto é: ${CostoBiglietto}€`);
+        // console.log(`Il costo del biglietto é: ${CostoBiglietto}€`);
 
     } else if (EtaPasseggero > 65){
         CostoBiglietto = ((0.21 * Chilometri) * 0.6).toFixed(2)
-        console.log(`Il costo del biglietto é: ${CostoBiglietto}€`);
+        // console.log(`Il costo del biglietto é: ${CostoBiglietto}€`);
 
     } else{
         CostoBiglietto = (0.21 * Chilometri).toFixed(2)
-        console.log(`Il costo del biglietto é: ${CostoBiglietto}€`);
+        // console.log(`Il costo del biglietto é: ${CostoBiglietto}€`);
     }
+
+document.querySelector(`#ticket`).classList.toggle(`d-none`)
 
 document.getElementById(`biglietto`).innerHTML = (`${CostoBiglietto}€`);
 
 document.getElementById(`nomeCognome`).innerHTML = (`${NomeCognome}`);
 
-let ticket = document.getElementById(`ticket`);
-ticket.style.display = "block";
+document.querySelector(`#carrozza`).innerHTML += `<span>${numeroRandomico(0,10)}</span>`;
+
+document.querySelector(`#codice`).innerHTML += `<span>${numeroRandomico(0,100000)}</span>`;
+
+function numeroRandomico(min, max){
+    return Math.floor(Math.random() * max) +min;
+}
+
 }
 
 
